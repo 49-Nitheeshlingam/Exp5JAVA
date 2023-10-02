985f5cc73ce348c7d83d9b73260f4c5fe88ab756
49-Nitheeshlingam <121334707+49-Nitheeshlingam@users.noreply.github.com>
2023-10-02 08:20:59 +0530

    Add files via upload

---
+++ property/ParentClass.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package property;
+
+/**
+ *
+ * @author DELL
+ */
+public class ParentClass {
+      protected String protectedproperty = "This is a protected property";
+    String defaultproperty = "This is a default property";
+}
